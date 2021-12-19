62fa225dac97416c55efc636d5d120cc9642db6d
Roberto881 <roberto881carlos881@hotmail.com>
2021-12-18 21:50:24 -0600

    Subida de archivos

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+export const navbar = `
+<div class="nav-bg">
+        <nav class="navegacion-principal contenedor">
+            <a href="index.html">Inicio</a>
+            <a href="lugares_visitar.html">Logares a Visitar</a>
+            <a href="mejores_hospedajes.html">Mejores Hospedajes</a>
+            <a href="transporte_economico.html">Transporte Economico</a>
+            <a href="paginas_voluntariado.html">Paginas de Voluntariado</a>
+            <a href="contacto.html">Contacto</a>
+            <a href="preguntas_frecuentes.html">Preguntas Frecuentes</a>
+        </nav>
+    </div>
+`;
